3d306ad4abafe2e5f90db4eca5086c0a514e8605
sanjeevkumarworks <144774750+sanjeevkumarworks@users.noreply.github.com>
2023-09-12 16:20:05 +0000

    did some basic coading

---
+++ index.js/index.js
@@ -0,0 +1,15 @@
+/*let products=[1,5,7,2,3,7];
+let minvalue=products[0];
+let maxProfit=0;
+for(let i=0;i<products.length;i++){
+let currentvalue=products[i];
+let currentProfit=currentvalue-minvalue;
+if(currentvalue<minvalue){
+    minvalue=currentvalue;
+}
+if(currentProfit> maxProfit){
+    maxProfit=currentProfit;
+}
+}
+console.log(maxProfit,minvalue);*/
+
